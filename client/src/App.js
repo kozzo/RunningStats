5e9edcb8c3f72e082b08ee5ef1bd5456541928d9
kozzo <kevin.zochowski@gmail.com>
2025-03-25 17:03:54 +0100

    react init

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './App.css';
+
+function App() {
+  const [runs, setRuns] = useState([]);
+
+  useEffect(() => {
+    axios.get('/runs')
+      .then(response => setRuns(response.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+       <div>
+      <h1>Running Stats</h1>
+      <ul>
+        {runs.map((run, index) => (
+          <li key={index}>
+            Date: {run.date} : Distance: {run.distance} km en {new Date(run.time * 1000).toISOString().slice(11, 19)} 
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
